acdfba96067aa2ea0ca8ec07e75912a1c758231a
Sajad <SajadSolidity@gmail.com>
2025-04-28 17:08:37 +0330

    Fixed

--- pumpswap/config/settings.py
+++ pumpswap/config/settings.py
@@ -1,17 +1,16 @@
-from pydantic import BaseModel, Field
+from pydantic import Field
 from dotenv import load_dotenv
+from pydantic_settings import BaseSettings
 
 load_dotenv()
 
-class Settings(BaseModel):
+class Settings(BaseSettings):
     """Global settings and bot configurations"""
-    wss_rpc_endpoint: str = Field(..., env="WSS_RPC_ENDPOINT")
     https_rpc_endpoint: str = Field(..., env="HTTPS_RPC_ENDPOINT")
-    
     buy_slippage: float = Field(0.3, env="BUY_SLIPPAGE")
     sell_slippage: float = Field(0.3, env="SELL_SLIPPAGE")
     swap_priority_fee: int = Field(1_500_000, env="SWAP_PRIORITY_FEE")
-    
+
     class Config:
         env_file = ".env"
-        env_file_encoding = 'utf-8'
+        env_file_encoding = "utf-8"

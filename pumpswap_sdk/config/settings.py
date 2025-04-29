978aeeed24f40bc21aab6ebb7f2522a2733e9402
Sajad <SajadSolidity@gmail.com>
2025-04-29 10:37:00 +0330

    Added dafault rpc

--- pumpswap_sdk/config/settings.py
+++ pumpswap_sdk/config/settings.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 class Settings(BaseSettings):
     """Global settings and bot configurations"""
-    https_rpc_endpoint: str = Field(..., env="HTTPS_RPC_ENDPOINT")
+    https_rpc_endpoint: str = Field("https://api.mainnet-beta.solana.com", env="HTTPS_RPC_ENDPOINT")
     buy_slippage: float = Field(0.3, env="BUY_SLIPPAGE")
     sell_slippage: float = Field(0.3, env="SELL_SLIPPAGE")
     swap_priority_fee: int = Field(1_500_000, env="SWAP_PRIORITY_FEE")

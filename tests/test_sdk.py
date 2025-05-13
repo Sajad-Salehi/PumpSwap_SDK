5571e19cb4a5354bd3d08a57189362f8eef8e5cf
Sajad <SajadSolidity@gmail.com>
2025-05-13 13:12:44 +0330

    updated

--- tests/test_sdk.py
+++ tests/test_sdk.py
@@ -7,8 +7,8 @@ from unittest.mock import patch, AsyncMock
 from pumpswap_sdk import PumpSwapSDK
 
 
-mint = "your-token-mint-address"  # Replace with a valid mint address on pumpswap
-user_private_key = "your-private-key"  # Replace with a valid private key
+mint = "8u8b6QfU6zXmKsyE34L5v96sTRHPcGXuweSEsGSNpump"  # Replace with a valid mint address on pumpswap
+user_private_key = "2Pbc1WsF7KZHzJU2QCtevutTT5hnkNu9yFAnR1ShPzhcmf2KoiXSAJLz7BuVBGKmYd4cZLKDLKA1aj7TGkBFqciL"  # Replace with a valid private key
 token_amount = 10.0 # Example token amount to sell
 sol_amount = 0.0001 # Example SOL amount to buy
 
@@ -18,7 +18,7 @@ async def test_buy():
     sdk = PumpSwapSDK()
 
     # Mock the buy_pumpswap_token function
-    with patch('pumpswap.core.buy_service.buy_pumpswap_token', new_callable=AsyncMock) as mock_buy:
+    with patch('pumpswap_sdk.core.buy_service.buy_pumpswap_token', new_callable=AsyncMock) as mock_buy:
         mock_buy.return_value = "buy_success"
 
         # Call the SDK method
@@ -34,7 +34,7 @@ async def test_sell():
     sdk = PumpSwapSDK()
 
     # Mock the sell_pumpswap_token function
-    with patch('pumpswap.core.sell_service.sell_pumpswap_token', new_callable=AsyncMock) as mock_sell:
+    with patch('pumpswap_sdk.core.sell_service.sell_pumpswap_token', new_callable=AsyncMock) as mock_sell:
         mock_sell.return_value = "sell_success"
 
         # Call the SDK method

163d4d72059095a2e427545358e331383722cadb
Sajad <SajadSolidity@gmail.com>
2025-06-11 12:51:30 +0330

    Fixed get pool data

--- pumpswap_sdk/core/pool_service.py
+++ pumpswap_sdk/core/pool_service.py
@@ -9,43 +9,55 @@ from pumpswap_sdk.utils.pool import PumpPool
 async def get_pumpswap_pair_address(mint_address: Pubkey):
 
     client: AsyncClient = await SolanaClient().get_instance()
-    filters = [
-        MemcmpOpts(offset=43, bytes=str(mint_address)),  # Matching mint address
-        MemcmpOpts(offset=75, bytes=str(constants.WSOL_TOKEN_ACCOUNT))  # Matching WSOL token account
+    search_orders = [
+        (mint_address, constants.WSOL_TOKEN_ACCOUNT),
+        (constants.WSOL_TOKEN_ACCOUNT, mint_address)
     ]
-    
-    response = await client.get_program_accounts(
-        constants.PUMP_AMM_PROGRAM_ID,
-        encoding="base64",  
-        filters=filters
-    )
 
-    if response.value:
-        pools = response.value[0].pubkey 
-        return pools
-    else:
-        return None
+    for token_a, token_b in search_orders:
+        filters = [
+            MemcmpOpts(offset=43, bytes=str(token_a)),
+            MemcmpOpts(offset=75, bytes=str(token_b)),
+        ]
+
+        response = await client.get_program_accounts(
+            constants.PUMP_AMM_PROGRAM_ID,
+            filters=filters,
+            encoding='base64'
+        )
+
+        if response.value:
+            pools = response.value[0].pubkey 
+            return pools
+        else:
+            return None
 
 
 
 async def get_pumpswap_pool_data(mint: Pubkey):
 
     client: AsyncClient = await SolanaClient.get_instance()
-    filters = [
-        MemcmpOpts(offset=43, bytes=str(mint)),
-        MemcmpOpts(offset=75, bytes=str(constants.WSOL_TOKEN_ACCOUNT)),
+    search_orders = [
+        (mint, constants.WSOL_TOKEN_ACCOUNT),
+        (constants.WSOL_TOKEN_ACCOUNT, mint)
     ]
 
-    resp = await client.get_program_accounts(
-        constants.PUMP_AMM_PROGRAM_ID, 
-        filters=filters, 
-        encoding='base64'
-    )
+    for token_a, token_b in search_orders:
+        filters = [
+            MemcmpOpts(offset=43, bytes=str(token_a)),
+            MemcmpOpts(offset=75, bytes=str(token_b)),
+        ]
 
-    if resp.value:
-        account_data = resp.value[0].account.data  # base64 encoded
-        binary_data = bytes(account_data)  
-        pool_data = PumpPool(binary_data)
-        return pool_data
-    else:
-        raise Exception("No matching pool found")
+        response = await client.get_program_accounts(
+            constants.PUMP_AMM_PROGRAM_ID,
+            filters=filters,
+            encoding='base64'
+        )
+
+        if response.value:
+            account_data = response.value[0].account.data  # base64 encoded
+            binary_data = bytes(account_data)  
+            pool_data = PumpPool(binary_data)
+            return pool_data
+
+    raise Exception("No matching pool found")
